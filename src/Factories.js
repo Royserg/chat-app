70ed988a18bf10da56a584e93f8ca4a5cb0fa564
Royserg <staniojakub@gmail.com>
2018-11-04 23:01:27 +0100

    Create object factories file

---
+++ src/Factories.js
@@ -0,0 +1,64 @@
+const uuidv4 = require('uuid/v4');
+
+/* Creating User object 
+*  @prop id {string}
+*  @prop name {string}
+*  @param {object}
+*     name {string}
+*/
+const createUser = ({name = ""} = {}) => (
+  {
+    id: uuidv4(),
+    name
+  }
+)
+
+/* Creating message object 
+*  @prop id {string}
+*  @prop time {Date} 24h format -> 17:38
+*  @prop message {string{
+*  @prop sender {string} name of message sender
+*  @param {object}
+*     message {string}
+*     sender {string}
+*/
+const createMessage = ({message = "", sender = ""} = {}) => (
+  {
+    id: uuidv4(),
+    time: getTime(new Date(Date.now())),
+    message,
+    sender
+  }
+)
+
+/* Creating chat object
+*  @prop id {string}
+*  @prop name {string}
+*  @prop messages {Array.string} 
+*  @prop users {Array.string}
+*  @prop typingUsers {Array.string}
+*  @param {object}
+*     messages {Array.Message}
+*     name {string}
+*     users {Array.string}
+*/
+const createChat = ({messages = [], name = "Community", users = []} = {}) => (
+  {
+    id: uuidv4(),
+    name,
+    messages,
+    users,
+    typingUsers: []
+    
+  }
+) 
+
+
+/* format time */
+const getTime = (date) => {
+  return `${date.getHours()}:${("0"+date.getMinutes()).slice(-2)}`;
+} 
+
+
+
+
